513208e2346b5805455d8a727d125aaf774bddb3
vbmcfarlane <vbmcfarlane@gmail.com>
2017-10-29 19:47:10 -0600

    heroku burger

--- config/connection.js
+++ config/connection.js
@@ -1,16 +1,15 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
-var connection = mysql.createConnection({
-  port: 3306,
-  host: "localhost",
-  user: "root",
-  password: "Abigail04$",
-  database: "burgers_db"
-});
-// Make connection.
-connection.connect(function(err) {
-  if (err) throw err;
-  console.log("connected as id " + connection.threadId);
-});
-// Export connection for our ORM to use.
+var connection;
+if (process.env.JAWSDB_URL) {
+	connection - mysql.createConnection(process.env.JAWSDB_URL);
+}else{
+	connection = mysql.createConnection({
+		host: 'localhost',
+		user: 'root',
+		password: 'hacktheplanet'
+		database: 'todoagain_db'
+	});
+};
+connection.connect();
 module.exports = connection;
