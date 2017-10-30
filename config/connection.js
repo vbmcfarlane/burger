df035b3ed7129b185407d14db3e71abfcc19ba14
vbmcfarlane <vbmcfarlane@gmail.com>
2017-10-30 00:24:16 -0600

    db

--- config/connection.js
+++ config/connection.js
@@ -5,10 +5,10 @@ if (process.env.JAWSDB_URL) {
 	connection - mysql.createConnection(process.env.JAWSDB_URL);
 }else{
 	connection = mysql.createConnection({
-		host: 'ko86t9azcob3a2f9.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
-		user: 'qmuhf79ni2feselj',
-		password: 'l64v55ganb8vtiku'
-		database: 'yvi6agkj0r2emub1'
+		host: 'localhost',
+		user: 'root',
+		password: 'hacktheplanet'
+		database: 'todoagain_db'
 	});
 };
 connection.connect();
