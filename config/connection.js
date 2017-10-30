e83dd67c9a624f554739c25f204791dd4ac930a7
vbmcfarlane <vbmcfarlane@gmail.com>
2017-10-30 11:59:30 -0600

    GitHub updt

--- config/connection.js
+++ config/connection.js
@@ -7,8 +7,8 @@ if (process.env.JAWSDB_URL) {
 	connection = mysql.createConnection({
 		host: 'localhost',
 		user: 'root',
-		password: 'hacktheplanet'
-		database: 'todoagain_db'
+		password: ''
+		database: 'burgers_db'
 	});
 };
 connection.connect();
